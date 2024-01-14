b2c41bc55ada6991bb33486af36c2cfbd458cdab
nothing <ddwi2504@gmail.com>
2024-01-14 18:17:56 +0800

    adawda

--- next.config.js
+++ next.config.js
@@ -4,7 +4,11 @@ const nextConfig = {
         remotePatterns: [{
             protocol: "https",
             hostname: "i.waifu.pics"
-        }]
+        },
+    {
+        protocol: "https",
+        hostname: "cdn.discordapp.com"
+    }]
     }
 }
 
